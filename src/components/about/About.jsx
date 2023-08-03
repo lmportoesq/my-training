de62218aba4b337bcaf9070921ba624def990e4d
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2023-08-03 08:00:02 -0500

    fix-warnings (#7)
    Co-authored-by: Luis Miguel Porto Esquivel <lmportoesq@gmail.com>

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,33 +1,39 @@
+import { Link } from 'react-router-dom';
+import './about.css';
+
 function About() {
-   function handleClickBack() {
+   /*function handleClickBack() {
       window.location.href = '/';
-   }
+   }*/
 
    return (
-      <div className='about-card'>
+      <div className='about'>
          <div>
             <h1>About me</h1>
             <hr />
          </div>
-         <div id="about-information">
-            <img src="./images/photo.jpg" alt="Photo" />
-            <div>
+         <div id="about__information">
+            <img src="./images/about-photo.jpg" alt="logo" />
+            <div id='about__information-text'>
                <h2>Luis Miguel Porto Esquivel</h2>
-            </div>
-            <div>
                <p>
-                  I am a professional in analysis and programming of computers,
-                  with more than 15 years of experience in the field of programming.
+                  I'm a professional in analysis and programming computers,
+                  with more than 15 years of experience in the programming field.
 
-                  Completely passionate about my profession, creative, easily adaptable to different work environments,
+                  Completely passionate about my profession, creative, easily adaptable to different works environments,
                   with the ability to lead, make decisions and work as a team.
                </p>
-               <div>
-                  <button className='button-back' onClick={handleClickBack}>Go back</button>
-               </div>
+            </div>
+            <div>
+               <Link to="/" className="button button--absolute">
+                  <span className="button__texto">Go back</span>
+                  <i className="button__icono fas fa-arrow-right"></i>
+               </Link>
             </div>
          </div>
       </div>
    )
 }
 export default About;
+
+//<button className='button-back' onClick={handleClickBack}>Go back</button>
